a9efa2807b1dc9e81b18af8f7feb9f237bdc75f5
Guilherme Alves <50877213+GuilhermeAMonte@users.noreply.github.com>
2020-07-18 03:30:18 -0300

    Add files via upload

---
+++ Numero Sorteado.py
@@ -0,0 +1,11 @@
+from random import randint
+import time
+
+n1 = int(input("Digite um numero aleatório para receber o valor do dado: "))
+valor = randint(1,6)
+print("Processando...")
+time.sleep(2)
+print("O valor sorteado foi {}!".format(valor))
+
+#primeiro projeto em Python (Guilherme Alves do Monte)
+
